64550c61961b751179141c20787e54c0eb35591f
diegowrhasta <diegowrhasta@gmail.com>
2019-02-09 15:04:17 -0400

    Nueva Prueba
    Intento para devolver objetos etiquetados

--- insertar.php
+++ insertar.php
@@ -17,14 +17,26 @@
         case 'registrarUsuario':
             $ci=filter_var($obj->ci, FILTER_SANITIZE_NUMBER_INT);
             $pass=filter_var($obj->pass, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
-            $nombre=filter_var($obj->nombre, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);   
+            $nombre=filter_var($obj->nombre, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
             $apellido=filter_var($obj->apellido, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
             $fecha_nac=filter_var($obj->fecha_nac, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
-            $estado='true';
-            $telf=filter_var($obj->telf, FILTER_SANITIZE_NUMBER_INT);
+            $estado=filter_var($obj->estado, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
+            $telf=filter_var($obj->telf, FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
             $sql="INSERT  INTO usuarios VALUES ('$ci','$nombre','$apellido','$fecha_nac','$estado','$pass','$telf')";
             $result=mysqli_query($conexion,$sql);
-            echo json_encode(array('message' => $result));
+            if($result==1){
+                $respuesta=array(
+                    'message' => 'OK',
+                    'ci' => '$ci',
+                    'nombre' => '$nombre',
+                    'apellido' => '$apellido'
+                );
+                echo json_encode($respuesta);
+            }else{
+                echo json_encode(array('message' => 'Fallo'));
+            }
+            
+            
         break;
         case 'registrarAuto':
             $placa=$_POST['placa'];
